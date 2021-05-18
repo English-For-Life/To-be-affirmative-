d73ad673c8d4a2997794bbabdea21a7b50af0571
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:02:37 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The weather ___ nice today.",
+        options:["-", "is", "are", "am"],
+        answer: 1
+    },
+    {
+     q:"This bag ___ heavy.",
+        options:["is", "are", "-", "am"],
+        answer: 0   
+    },
+    {
+        q:"These bags ___ heavy.",
+        options:["am", "is", "-", "are"],
+        answer: 3
+    },
+    {
+        q:"Look! There ___ Linda.",
+        options:["are", "-", "am", "is"],
+        answer: 3
+    },
+    {
+        q:"My sister and I ___ good tennis players.",
+        options:["-", "is", "are", "am"],
+        answer: 2
+    },
+    {
+        q:"I ___ at home.",
+        options:["are", "am", "-", "is"],
+        answer: 1
+    },
+    {
+        q:"Her children ___ at school.",
+        options:["are", "-", "is", "am"],
+        answer: 0
+    },
+    {
+        q:"She is very old woman. She ___ 94.",
+        options:["are", "am", "-", "is"],
+        answer: 3
+    },
+    {
+        q:"I ___ from Italy.",
+        options:["-", "am", "is", "are"],
+        answer: 1
+    },
+    {
+        q:"These flowers ___ beautiful.",
+        options:["is", "am", "are", "-"],
+        answer: 2
+    }
+
+]
